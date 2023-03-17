b59cd7d580f5f063324e7277d366ee16eace4128
Norajeout <norajeout123@gmail.com>
2023-03-16 18:27:42 -0700

    yaaw

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,14 +4,13 @@
  * Return: 1
  */
 int main(void)
-{
-        char str[] = "and that piece of art is useful" - Dora Korpar, 2015-10-19\n";
-        int i = 0;
+{ char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+int i = 0;
 
-        while (str[i] != '\0')
-        {
-                putchar(str[i]);
-                i++;
-        }
-        return (1);
+while (str[i] != '\0')
+{
+putchar(str[i]);
+i++;
+}
+return (1);
 }
