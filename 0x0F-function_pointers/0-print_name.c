453c5a54e77a24a3434561d1b5c7b95ea84ce4d3
Norajeout <norajeout123@gmail.com>
2023-04-19 12:56:28 -0700

    print name

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,12 +1,12 @@
 #include "function_pointers.h"
 #include <stdio.h>
 /* by nora jeout */
-/*
- * print_name - print name using pointer to function
- * @f : pointer to function
- * @name : string to add
- * Return: void
- */
+/**
+* print_name - print name using pointer to function
+* @name: string to add
+* @f: pointer to function
+* Return: nothing
+**/
 void print_name(char *name, void (*f)(char *))
 	{
 		if (!name || !f)
