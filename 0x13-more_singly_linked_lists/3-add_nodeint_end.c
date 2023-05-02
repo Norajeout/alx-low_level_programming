2224cc3129f3d7af0a465361e32ccacac2434255
Norajeout <norajeout123@gmail.com>
2023-05-02 16:28:15 -0700

    nodeint

---
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+/* nora je out */
+/**
+ * add_nodeint_end - adds a new node at the end of a listint_t list
+ * @head: parameter
+ * @n: parameter
+ * Return: results
+ */
+listint_t *add_nodeint_end(listint_t **head, const int n)
+{
+	listint_t *new, *temp;
+
+	new = malloc(sizeof(listint_t));
+	if (!new)
+		return (NULL);
+	new->n = n;
+	new->next = NULL;
+	if (*head == NULL)
+	{
+		*head = new;
+		return (new);
+	}
+	temp = *head;
+	while (temp->next)
+		temp = temp->next;
+	temp->next = new;
+	return (new);
+}
