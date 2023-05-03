baaa81b5176b5614c3b095c6d1ba27cb715ce697
Norajeout <norajeout123@gmail.com>
2023-05-02 17:12:46 -0700

    lists

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -1,27 +1,23 @@
 #include "lists.h"
-/* nora jeout*/
 /**
- * reverse_listint - reverses a listint_t linked list.
- * @head: parameter
- * Return: results
- */
+* *reverse_listint - reverse a list
+* @head: pointer
+* Return: reversed list
+*/
+/* nora je out*/
 listint_t *reverse_listint(listint_t **head)
 {
-	listint_t *ls = NULL;
-	listint_t *ls2 = NULL;
-	int i = 0;
-
-	if (!head)
-		return (NULL);
+	listint_t *prev = NULL;
+	listint_t *next = NULL;
 	while (*head)
 	{
-		ls2 = (*head)->next;
-		(*head)->next = ls;
-		ls = *head;
-		*head = ls;
-		*head = ls2;
-		i++;
+		next = (*head)->next;
+		(*head)->next = prev;
+		prev = *head;
+		*head = next;
+
 	}
-	*head = ls;
+
+	*head = prev;
 	return (*head);
 }
