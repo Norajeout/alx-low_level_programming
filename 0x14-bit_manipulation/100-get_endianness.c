7ada6f2b4ce94346171577e7924cf3d57db88ef4
Norajeout <norajeout123@gmail.com>
2023-05-04 15:59:41 -0700

    manipulation is better

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/* nora jeout */
+/**
+ * get_endianness - Entry point
+ * Return: int
+ */
+
+int get_endianness(void)
+{
+	int n = 1;
+
+	if (*(char *)&n == 1)
+		return (1);
+	else
+		return (0);
+}
