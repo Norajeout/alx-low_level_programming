a65ff76469f6b8a0b0596cff910df377fd787def
Norajeout <norajeout123@gmail.com>
2023-05-08 17:15:36 -0700

    I/O

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,39 +1,30 @@
 #include "main.h"
-/* nora jeout */
+#include <stdlib.h>
+/*nora jeout */
+
 /**
- * read_textfile - reads a text file and prints it to the POSIX
- * @filename: parameter
- * @letters: parameter
- * Return: result
+ * read_textfile- Read text file print to STDOUT.
+ * @filename: text file being read
+ * @letters: number of letters to be read
+ * Return: w- actual number of bytes read and printed
+ *        0 when function fails or filename is NULL.
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int dod, dmg, meta;
-	char *stck;
+	char *stuck;
+	ssize_t of;
+	ssize_t w;
+	ssize_t r;
 
-	if (!filename)
+	of = open(filename, O_RDONLY);
+	if (of == -1)
 		return (0);
-	dod = open(filename, O_RDONLY);
-	dod = open(filename, O_RDONLY);
-	if (dod < 0)
-		return (0);
-	stck = (char *) malloc(letters * sizeof(char));
-	if (!stck)
-		return (0);
-	dmg = read(dod, stck, letters);
-	if (dmg < 0)
-	{
-		free(stck);
-		return (0);
-	}
-	stck[dmg] = '\0';
-	meta = write(STDOUT_FILENO, stck, dmg);
-	if (meta < 0)
-	{
-		free(stck);
-		return (0);
-	}
-	free(stck);
-	close(dod);
-	return (meta);
+	stuck = malloc(sizeof(char) * letters);
+	r = read(of, stuck, letters);
+	w = write(STDOUT_FILENO, stuck, r);
+
+	free(stuck);
+	close(of);
+	return (w);
 }
+
